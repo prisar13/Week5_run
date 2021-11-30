35e887436b186b0c39660780b1c4b900327ef1a3
Priyanka Sarkar <prisar13@gmail.com>
2021-11-30 20:58:49 +0530

    updated

--- script.js
+++ script.js
@@ -4,17 +4,20 @@ document.addEventListener("DOMContentLoaded",
 
 		//Unobstrusive event binding
 		document.querySelector("button").addEventListener("click",function(){
-			var self = this;
-			var name = "";
+			// var self = this;
+			// var name = "";
 
 			// Call server to get the name
 			$ajaxUtils.sendGetRequest("name.txt",
 				function(request){
-					self.name = request.responseText;
-					console.log(self.name);
+					// self.name = request.responseText;
+					var name = request.responseText;
+
+					document.querySelector("#content").innerHTML =
+					"<h2>Hello "+ name +"!</h2>";
 				});
-			document.querySelector("#content").innerHTML=
-			"<h2>Hello "+self.name+"!</h2>";
+			// document.querySelector("#content").innerHTML=
+			// "<h2>Hello "+self.name+"!</h2>"; Asynchronous so this part needs to be inside function
 		});
 	}
 	);
